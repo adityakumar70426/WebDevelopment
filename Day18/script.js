a261687f2141f822d03548ca259d619647c57ca5
adityakumar70426 <adityakumar70426@gmail.com>
2025-06-29 18:37:11 +0530

    day18

---
+++ Day18/script.js
@@ -0,0 +1,31 @@
+//monitor
+monitorEvents(document);
+
+//unmonitor
+unmonitorEvents(document);
+
+//eventlistener
+
+function print(){
+    console.log("you clicked");
+};
+
+//addeventlistener
+document.addEventListener('click',print);
+
+//remove eventlistener
+document.removeEventListener('click',print);
+
+// info. of event
+let c=document.querySelector('#wrapper');
+c.addEventListener('click',function(event){
+    console.log(event);
+});
+
+//prevent Default action
+let link=document.querySelector('a');
+link[0].addEventListener('click',function(aditya){
+    aditya.preventDefault;
+    console.log('nhi chala');
+});
+
